59b3b3298bbf9e564406d42d3f6f79e798e33591
Mogrima <rebel.rs@yandex.ru>
2024-01-01 05:07:15 +0500

    в род классе взрывов в методе update реализовано выполнение анимации в зависимости от заданного в конструктуре кол-ва кадров анимации, в методе draw дописаны все переменные для метода draw.Image

--- src/Explosions/Explosion.js
+++ src/Explosions/Explosion.js
@@ -7,15 +7,29 @@ class Explosion {
         // у всех фреймов анимации одинаковая высота, но будет разная ширина
         this.spriteHeight = 200;
         // увеличим время анимации каждого кадра, так как количество фреймов достаточно маленькое
-        this.fps = 15;
+        // фактически сами задаем количество кадров анимации, например, уменьшая его, если кол-во
+        // фреймов недостаточно, а общее число кадров игры, заданное в deltatime больше
+        this.fps = 30;
         this.timer = 0;
         this.interval = 1000/this.fps;
         this.markedForDeletion = false;
+        this.maxFrame = 8;
     }
     update(deltaTime) {
-        this.frameX++;
+        // анимация будет прокручиваться вместе с движением игрового мира
+        this.x -= this.game.speed;
+        if(this.timer > this.interval) {
+            this.frameX++;
+            this.timer = 0;
+        } else {
+            this.timer += deltaTime;
+        }
+        
+        if(this.frameX > this.maxFrame) this.markedForDeletion = true;
     }
     draw(context) {
-        context.drawImage(this.image, this.x, this.y);
+        context.drawImage(this.image, this.frameX * this.spriteWidth, 0,
+                        this.spriteWidth, this.spriteHeight, this.x, this.y,
+                        this.width, this.height);
     }
 }
